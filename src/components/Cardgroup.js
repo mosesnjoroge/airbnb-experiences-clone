0f53fd0b92baf3e385d99710a53bdea530e96e43
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-07 04:00:03 -0500

    card group code structure

--- src/components/Cardgroup.js
+++ src/components/Cardgroup.js
@@ -1,53 +1,52 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 export default function Cardgroup () {
 
   return (
-    <div className = 'card--group'>
-      <Container>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Life lessons with katie Zaferes
-            </Card.Text>
-            <Card.Text>
-            <strong> From $136</strong> / person
-            </Card.Text>
-          </Card.Body>
-        </Card>
+    <CardGroup className = 'container'>
+      <Card >
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Rating goes here - </Card.Title>
+          <Card.Text>
+            Life lessons with katie Zaferes
+          </Card.Text>
+          <Card.Text>
+          <strong> From $136</strong> / person
+          </Card.Text>
+        </Card.Body>
+      </Card>
 
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Learn Wedding Photography
-            </Card.Text>
-            <Card.Text>
-            <strong> From $125</strong> / person
-            </Card.Text>
-          </Card.Body>
-        </Card>
+      <Card >
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Rating goes here -</Card.Title>
+          <Card.Text>
+            Learn Wedding Photography
+          </Card.Text>
+          <Card.Text>
+          <strong> From $125</strong> / person
+          </Card.Text>
+        </Card.Body>
+      </Card>
 
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Group Mountain Biking
-            </Card.Text>
-            <Card.Text>
-              <strong> From $50</strong> / person
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Rating goes here -</Card.Title>
+          <Card.Text>
+            Group Mountain Biking
+          </Card.Text>
+          <Card.Text>
+            <strong> From $50</strong> / person
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardGroup>
 
-    </div>
+
 
   );
 
