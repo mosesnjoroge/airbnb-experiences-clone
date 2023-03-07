fb77f9da4d46eb202f649b208c8664f705d71786
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-07 04:57:17 -0500

    second commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,13 +6,11 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function Nav () {
 
   return (
-
     <Navbar bg = 'light'>
       <Container>
         <Navbar.Brand href='#home'>AirBnb</Navbar.Brand>
       </Container>
     </Navbar>
-
-  )
+  );
 
 }
