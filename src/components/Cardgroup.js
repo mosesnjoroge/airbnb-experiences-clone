6a490b921b01cb11b22e545bc9959f453a697b66
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-08 14:38:14 -0500

    cloudinary sample photo use

--- src/components/Cardgroup.js
+++ src/components/Cardgroup.js
@@ -2,21 +2,35 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import {Cloudinary} from "@cloudinary/url-gen";
+import {AdvancedImage} from '@cloudinary/react';
+import {fill} from "@cloudinary/url-gen/actions/resize";
+
+
 
 export default function Cardgroup () {
 
   // Create a Cloudinary instance and setting cloud name.
   const cld = new Cloudinary({
     cloud: {
-      cloudName: 'dtxxea3qi'
+      cloudName: 'demo'
     }
   });
+
+  // Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
+   const myImage = cld.image('docs/models');
+
+  // Resize to 250 x 250 pixels using the 'fill' crop mode.
+   myImage.resize(fill().width(250).height(250));
+
+
 
 
   return (
     <CardGroup className = 'container'>
       <Card>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top">
+          <AdvancedImage cldImg={myImage} />
+        </Card.Img>
         <Card.Body>
           <Card.Title>Rating goes here - </Card.Title>
           <Card.Text>
