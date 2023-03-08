c64c1a92a7401a60aaad9b45d469b1ca158ccf2e
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-08 14:25:47 -0500

    cloudinary setup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Cardgroup from './components/Cardgroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Cloudinary} from "@cloudinary/url-gen";
 
 function App() {
+  // Create a Cloudinary instance and set your cloud name.
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dtxxea3qi'
+    }
+  });
+
   return (
     <div className="App">
       <Nav />
       <Hero />
-      <Cardgroup />
+      <Cardgroup>
+      </Cardgroup>
     </div>
   );
 }
