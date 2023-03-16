282e56a856ccd03453059711984945d4a4cfacd8
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-16 18:01:00 -0400

    adding the map method to appjs and removing the instance statements in the return statement

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import Nav from './components/Nav';
 import Hero from './components/Hero';
 import ExperienceCards from './components/Experiencecards';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import data from './data'
 
 function App() {
+
+  const experienceElements = data.map(experience => {
+
+    return (
+
+      <ExperienceCards
+        id = {experience.id}
+        availablity = {experience.availablity}
+        rating = {experience.rating}
+        status = {experience.status}
+        caption = {experience.caption}
+        price = {experience.price}
+      />
+     )
+    })
 
   return (
     <div className="App">
       <Nav />
       <Hero />
-      <ExperienceCards
-
-      />
-
+      {experienceElements}
     </div>
   );
 }
